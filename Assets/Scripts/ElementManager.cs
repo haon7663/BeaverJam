c6154bdc1705ee4231f5d77042680e723a2a214e
haon7663 <89632611+haon7663@users.noreply.github.com>
2023-08-19 03:29:57 +0900

    ElementalManager

--- Assets/Scripts/ElementManager.cs
+++ Assets/Scripts/ElementManager.cs
@@ -2,18 +2,34 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-enum Element { Vapor, Water, Ice }
+public enum Element { Vapor, Water, Ice }
 
 public class ElementManager : MonoBehaviour
 {
     public static ElementManager Inst { get; private set; }
 
-    [SerializeField] Element element;
+    public Element element;
+
+    public float elementalEnergy;
 
     void Awake() => Inst = this;
 
-    public void SetElement(int elementCount)
+    public bool SetElement(int elementCount)
     {
+        if (elementCount < 0 || elementCount > 2)
+            return false;
         element = (Element)elementCount;
+        return true;
+    }
+
+    public void ChargeEnergy(float value)
+    {
+        elementalEnergy += value;
+        if (elementalEnergy > 100) elementalEnergy = 100;
+    }
+
+    public void ZeroEnergy()
+    {
+        elementalEnergy = 0;
     }
 }
