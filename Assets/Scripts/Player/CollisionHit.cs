7feff7b63784d89ce8a3c75a543693a6f47df413
dunfr <j01026708401@gmail.com>
2023-08-19 23:49:11 +0900

    rockslideTrap

--- Assets/Scripts/Player/CollisionHit.cs
+++ Assets/Scripts/Player/CollisionHit.cs
@@ -5,16 +5,18 @@ using UnityEngine;
 public class CollisionHit : MonoBehaviour
 {
     [SerializeField] LayerMask groundLayer;
+    [SerializeField] LayerMask rockSlideLayer;
     [SerializeField] Vector3 buttomOffset, topOffset, rightOffset, leftOffset;
     [SerializeField] float radious;
 
-    public bool onGround, onWall;
+    public bool onGround, onWall, onHead;
 
     void Update()
     {
         onGround = Physics2D.OverlapCircle(transform.position + buttomOffset, radious, groundLayer);
         onWall = Physics2D.OverlapCircle(transform.position + leftOffset, radious, groundLayer) ||
                  Physics2D.OverlapCircle(transform.position + rightOffset, radious, groundLayer);
+        onHead = Physics2D.OverlapCircle(transform.position + topOffset, radious, rockSlideLayer);
     }
 
     void OnDrawGizmos()
@@ -24,5 +26,6 @@ public class CollisionHit : MonoBehaviour
         Gizmos.DrawWireSphere(transform.position + buttomOffset, radious);
         Gizmos.DrawWireSphere(transform.position + leftOffset, radious);
         Gizmos.DrawWireSphere(transform.position + rightOffset, radious);
+        Gizmos.DrawWireSphere(transform.position + topOffset, radious);
     }
 }
