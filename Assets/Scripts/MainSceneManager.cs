da62d2a2bb6e72031a4051950923f85a97af005d
dlselrpa <dlselrpa@naver.com>
2023-08-20 03:31:46 +0900

    Main conplite

--- Assets/Scripts/MainSceneManager.cs
+++ Assets/Scripts/MainSceneManager.cs
@@ -5,8 +5,22 @@ using UnityEngine.SceneManagement;
 
 public class MainSceneManager : MonoBehaviour
 {
-    public void OnClickPlay()
+    public void OnClickContinue()
     {
         SceneManager.LoadScene("Scenes/InGameScene");
+    }
+    
+    public void OnClickStartFresh()
+    {
+        SaveManager.Inst.Delete();
+        SceneManager.LoadScene("Scenes/InGameScene");
+    }
+    public void OnClickExit()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+        Application.Quit();
+#endif
     }
 }
