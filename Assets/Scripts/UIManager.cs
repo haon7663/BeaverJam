61d54ae1f7769dd97a650431c3265292dcdb6c1d
haon7663 <89632611+haon7663@users.noreply.github.com>
2023-08-20 09:10:37 +0900

    UIManager

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
+using DG.Tweening;
 
 public class UIManager : MonoBehaviour
 { 
@@ -15,8 +16,6 @@ public class UIManager : MonoBehaviour
     [SerializeField] RectTransform[] elementalGauge;
     [SerializeField] RectTransform elementRect;
 
-    List<int> elements = new List<int>();
-
     Vector2[] saveScale = new Vector2[3];
 
     void Awake() => Inst = this;
@@ -25,14 +24,64 @@ public class UIManager : MonoBehaviour
     {
         for(int i = 0; i < 3; i++)
         {
-            elements.Add((int)ElementManager.Inst.element);
             saveScale[i] = elementalGauge[i].sizeDelta;
         }
+        ChangeElement();
     }
+
 
     public void ChangeElement()
     {
+        for (int i = 0; i < 3; i++)
+        {
+            Color color = Color.white;
+            var elementCount = Elemental.saveElement;
+            var distance = elementCount == i ? 0 : elementCount > i ? elementCount - i : i - elementCount;
+            var mDistance = i - elementCount;
+            if (distance == 0)
+                elementalGauge[i].SetAsLastSibling();
+            else
+                elementalGauge[i].SetAsFirstSibling();
 
+            Vector3 localSave = new Vector3(mDistance * 100, 0);
+            elementalGauge[i].DOLocalMove(localSave, 0.1f);
+            elementalGauge[i].DOSizeDelta(new Vector3(saveScale[i].x / (1 + (float)distance / 2), saveScale[i].y / (1 + (float)distance / 2)), 0.1f);
+            if(i != 2)
+            {
+                elementalGauge[i].transform.GetChild(0).GetComponent<Image>().DOFade(distance == 0 ? 1 : distance == 1 ? 0.2f : 0f, 0.1f);
+                elementalGauge[i].transform.GetChild(0).GetChild(0).GetComponent<Image>().DOFade(distance == 0 ? 1 : distance == 1 ? 0.2f : 0f, 0.1f);
+            }
+
+            if (elemental.isChange)
+            {
+                if (i == 2)
+                    for (int j = 0; j < 6; j++)
+                        elementalGauge[2].transform.GetChild(j).GetChild(0).GetComponent<Image>().DOFade(distance == 0 ? 1 : distance == 1 ? 0.2f : 0f, 0.1f);
+            }
+            else
+            {
+                var j = Elemental.saveElement;
+                if (i != 2)
+                {
+                    DOTween.To(() => elementalGauge[j].transform.GetChild(0).GetChild(0).GetComponent<Image>().fillAmount, 
+                        x => elementalGauge[j].transform.GetChild(0).GetChild(0).GetComponent<Image>().fillAmount = x, 
+                        (float)ElementManager.Inst.elementalEnergy / 6, 0.5f);
+                }
+                if (j == 2)
+                {
+                    for (int k = 0; k < 6; k++)
+                    {
+                        elementalGauge[j].transform.GetChild(k).GetChild(0).GetComponent<Image>().DOFade(ElementManager.Inst.elementalEnergy > k ? 1 : 0f, 0.5f);
+                    }
+                }
+            }
+
+            if (i == 2)
+            {
+                for (int j = 0; j < 6; j++)
+                    elementalGauge[2].transform.GetChild(j).GetComponent<Image>().DOFade(distance == 0 ? 1 : 0.2f, 0.1f);
+            }
+        }
     }
 
     private void Update()
